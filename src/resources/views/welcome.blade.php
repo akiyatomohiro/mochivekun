7c7305c6c8e1cbdbc59caa43ef4c57003a09660d
akiyatomohiro <thairwars@gmail.com>
2023-04-27 13:07:35 +0900

    # Reactを使う

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,36 +1,11 @@
-<!-- <!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Fullstack Project</title>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
-</head>
-<body>
-    <div id="example">sagv</div>
-    <script src="{{ mix('/js/app.js') }}"></script>
-</body>
-</html> -->
-
-<!-- <!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Fullstack Project</title>
-    <link rel="stylesheet" href="/src/resources/css/app.css" />
-</head>
-<body>
-    <div id="example">sagv</div>
-    <script type="module" src="/src/resources/js/app.js"></script>
-</body>
-</html> -->
-
 <!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        @vite(['resources/css/app.css', 'resources/ts/app.ts'])
+        @viteReactRefresh
+        @vite(['resources/css/app.css', 'resources/ts/app.tsx'])
     </head>
     <body>
         <div id="app"></div>
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
- 
+
 export default defineConfig({
     server: {
         host: true,
-        hmr: { 
+        hmr: {
             host: 'localhost'
         },
         watch: {
@@ -14,7 +14,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/ts/app.ts'],
+            input: ['resources/css/app.css', 'resources/ts/app.tsx'],
             ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
